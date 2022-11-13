0ec2769114584bb95f227f81407ab4220c52c869
league <league@imac-24.attlocal.net>
2022-11-12 16:28:31 -0800

    Finish the Duck code

--- src/_06_vault/VaultRunner.java
+++ src/_06_vault/VaultRunner.java
@@ -2,10 +2,18 @@ package _06_vault;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class VaultRunner {
 	public static void main(String[] args) {
 		Vault cracker = new Vault();
 		int guess = new Random().nextInt(1000001);
-		cracker.tryCode(int guess);
+		while (cracker.tryCode(guess) == false) {
+		
+			JOptionPane.showMessageDialog(null, "The Vault has been unlocked, Congratz ");
+			
+		
+			cracker.tryCode(guess);
+		}
 	}
 }
